976bc61454ccd8f8e638f04d96476dec47277f3e
nospame <mapson.evan@gmail.com>
2022-04-14 11:18:57 -0700

    add notes components

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,18 @@
+<script>
+import NoteSelectors from './NoteSelectors.vue'
+import NoteEditor from './NoteEditor.vue'
+export default {
+  components: {
+    NoteSelectors,
+    NoteEditor
+  }
+}
+
+</script> 
+ 
+<template>
+  <div class="note-container">
+    <NoteSelectors />
+    <NoteEditor />
+  </div>
+</template>
---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,14 @@
+<script>
+import NoteSelector from './NoteSelector.vue'
+export default {
+  components: {
+    NoteSelector
+  }
+}
+</script>
+
+<template>
+  <div class="note-selectors">
+    <NoteSelector />
+  </div>
+</template>
